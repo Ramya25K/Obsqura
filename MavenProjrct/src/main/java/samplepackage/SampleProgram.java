fe1561d1852dc290d5ef8ac61d296592ada9a849
User <User@DESKTOP-3GSOFRD>
2024-06-24 22:53:36 +0530

    commit

---
+++ MavenProjrct/src/main/java/samplepackage/SampleProgram.java
@@ -0,0 +1,11 @@
+package samplepackage;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Ramya!");
+
+	}
+
+}
